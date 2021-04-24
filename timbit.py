991efd24aed5368bb8ec09eef174bb5caa7af9bc
Code Jones <mdjones1793@gmail.com>
2021-04-23 21:11:04 -0600

    tried argparser

--- timbit.py
+++ timbit.py
@@ -1,19 +1,31 @@
-import sys
-import os
-import click
+# import click
+#
+# @click.group()
+# @click.pass_context
+# def timbit(self):
+#     pass
+#
+# @timbit.command()
+# @timbit.option('--name', prompt='Your name', help='The help')
+# def test(name):
+#     print(f"My name is {name}")
+#
+# cli = click.CommandCollection(sources=[timbit])
 
-@click.group()
-@click.pass_context
-def timbit(self):
-    pass
+import argparse
 
-@timbit.command()
-def test():
-    print("working")
+def timbit():
+    parser = argparse.ArgumentParser(description="What da program do")
 
-cli = click.CommandCollection(sources=[timbit])
+    parser.add_argument("arg1", help="advice on arg1")
+    parser.add_argument("arg2", help="advice on arg2")
+
+    args = parser.parse_args()
+    print(args)
 
 if __name__ == '__main__':
     timbit()
+
+
 
 
